6d5324f17f151b370307c0df1288bf25a3c79753
andresfmpaz <andresfmpaz@gmail.com>
2024-04-11 13:23:14 -0500

    initial commit py_platzy

---
+++ 01_sets.py
@@ -0,0 +1,29 @@
+set_countries ={'col','mex','bol','col','Col'}
+print(set_countries)
+print(type(set_countries))
+
+set_numbers ={1,33,4,1,444}
+print(set_numbers)
+print(type(set_numbers))
+
+
+set_types ={1,33,4,1,444,'dlll', 0.1,False}
+print(set_types)
+print(type(set_types))
+
+tup_numbers = ('aa' , 1 , False , 'fff', 'aa')
+
+print(tup_numbers)
+
+print(type(tup_numbers))
+
+set_strings = set('este es un mundo muy muy interesante')
+print(set_strings)
+print(type(set_strings))
+
+
+print('convirtiendo tupla a set')
+
+set_tuple = set(tup_numbers)
+print(set_tuple)
+print(type(set_tuple))
---
+++ 02_crud_set.py
@@ -0,0 +1,21 @@
+set_countries = {'col','mex','bol'}
+
+size = len(set_countries)
+print(f' el tamano es {size}')
+
+print('col' in set_countries)
+
+print('pe' in set_countries)
+
+print(set_countries.add('pe'))
+print(set_countries)
+print('peru esta en el conjunto ', 'pe' in set_countries)
+print('antes update',set_countries)
+#actualizar un conjunto con update
+set_countries.update({'arg', 'bol', 'urg', 'cl'})
+
+print('despues update',set_countries)
+
+#print('remove ',set_countries.remove('ar'))
+print('discard ',set_countries.discard('arg'))
+print('despues del discard',set_countries)
