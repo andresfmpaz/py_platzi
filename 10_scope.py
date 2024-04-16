0983c14fec20cc9faffb3fa4292aab3244775749
andresfmpaz <andresfmpaz@gmail.com>
2024-04-16 15:41:07 -0500

    :+1: 10 scope learning

---
+++ 10_scope.py
@@ -0,0 +1,12 @@
+price = 100 # global inside the file
+
+def increment():
+    price = 200
+    price = price + 10
+    print(price)
+    return price
+    
+print(price)
+
+price_2 = increment()
+print('price2', price_2)
