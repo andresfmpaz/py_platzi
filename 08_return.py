15ee3a762d467f7180ca50d8a7db154a10568abb
andresfmpaz <andresfmpaz@gmail.com>
2024-04-16 11:04:42 -0500

    :v: we have add a function with return 08_return

---
+++ 08_return.py
@@ -0,0 +1,15 @@
+
+def sum_with_range(min, max):
+    sum = 0
+    for x in range(min, max):
+        sum += x
+    return sum
+
+result = sum_with_range(1, 5)
+print(result)
+result = sum_with_range(1,10)
+print(result)
+result = sum_with_range(1, 15)
+print(result)
+result = sum_with_range(1, 20)
+print(result)
