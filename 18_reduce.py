67116facad21b0de6318b2307804fb6fa8e87a4e
andresfmpaz <andresfmpaz@gmail.com>
2024-04-26 09:10:35 -0500

    :v: working with reduce

---
+++ 18_reduce.py
@@ -0,0 +1,31 @@
+import functools
+
+numbers = [1, 2, 3, 4]
+
+result = functools.reduce(lambda counter, item : counter + item, numbers)
+
+print(result)
+
+numbers_2 = [2, 4, 6, 8]
+
+def acum(counter, item):
+    print('counter -> ',counter)
+    print('item -> ',item)
+    return counter + item
+
+result = functools.reduce(acum, numbers)
+
+
+
+print(result)
+
+def acum_1(counter = 10, item):
+    print('counter -> ',counter)
+    print('item -> ',item)
+    return counter - item
+
+result = functools.reduce(acum_1, numbers)
+
+
+
+print(result)
