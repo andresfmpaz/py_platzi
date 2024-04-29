a077118f6ee9b98db03bc4b7061fe74c62d9cd81
andresfmpaz <andresfmpaz@gmail.com>
2024-04-29 10:31:59 -0500

    :v: without any value in the function

---
+++ pkg/mod_1.py
@@ -0,0 +1,5 @@
+def func_1():
+    return 'mod_1 : func_1'
+
+def func_2():
+    return 'mod_1 : func_2'
---
+++ pkg/mod_2.py
@@ -0,0 +1,5 @@
+def func_3():
+    return 'mod_2 : func_3'
+
+def func_4():
+    return 'mod_2 : func_4'
